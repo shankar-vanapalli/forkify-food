a7d8a76e66c17f23b0a758c58eb3b0f4b1f58748
Bhavani Shankar Vanapalli <40032387+shankar067@users.noreply.github.com>
2022-06-28 15:01:32 +0530

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6fc0faea-0c6d-4f24-9ebd-67c47b6f044f';
+export const MODAL_CLOSE_SEC = 2.5;
